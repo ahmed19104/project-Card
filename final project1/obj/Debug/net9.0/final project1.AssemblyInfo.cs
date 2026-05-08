62d3b7e7219bad7fe5002a5d6d2355d4434e960b
AHMED YASSER <a3939361@gmail.com>
2026-05-08 19:27:25 +0300

    View

--- final project1/obj/Debug/net9.0/final project1.AssemblyInfo.cs
+++ final project1/obj/Debug/net9.0/final project1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("final project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92db0d341a699bf61f25d3fa85e2dd8a9d9433dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("final project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("final project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
